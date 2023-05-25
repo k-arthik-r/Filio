b6d2c0f17f390c3ca17cfae87fa0d89c0133fec3
Karthik R <karthikclgid@gmail.com>
2023-05-25 13:03:23 +0530

    add geometry icon and title

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import os
 import shutil
 
 import tkinter as tk
+
+root = tk.Tk()
+
+icon = tk.PhotoImage(file= 'icon.png')
+
+root.geometry("500x500")
+root.iconphoto(False, icon)
+root.title("Filio")
+
+
+root.mainloop()
