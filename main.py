8b7ee00509bb3c8fd18454be1bd16f481a22e25e
Karthik R <karthikclgid@gmail.com>
2023-05-25 15:05:39 +0530

    adding logo

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import shutil
-
 import tkinter as tk
 
 root = tk.Tk()
 
 icon = tk.PhotoImage(file= './assets/icon.png')
 bg = tk.PhotoImage(file='./assets/background.png')
-logoname = tk.PhotoImage(file='./assets/logo.png')
+logoname = tk.PhotoImage(file='./assets/logoname.png')
+logoimage = tk.PhotoImage(file='./assets/logoimage.png') 
 
 root.title("Filio")
 root.iconphoto(False, icon)
@@ -18,6 +18,7 @@ root.resizable(False, False)
 background = tk.Label(root, image=bg)
 background.pack()
 
-logo = tk.Label(root, image=logoname, bg='#ffff00').place(x=20, y=20)
+logoa = tk.Label(root, image=logoname, bg='#ffff00').place(x=20, y=20)
+logob = tk.Label(root, image=logoimage, bg='#ffff00').place(x=60, y=120)
 
 root.mainloop()
